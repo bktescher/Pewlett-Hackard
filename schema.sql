ea365adf55627317fa76752a2c48e9fc62dcbd4e
Brett <bnb@bnb.local>
2022-02-11 14:37:16 -0500

    7.3.1 complete

--- schema.sql
+++ schema.sql
@@ -38,11 +38,13 @@ CREATE TABLE salaries (
 
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
+
 );
 
 CREATE TABLE titles(
@@ -51,7 +53,7 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
+
 );
 
-SELECT * FROM departments;
